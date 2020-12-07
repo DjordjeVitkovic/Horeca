af589fa4558de05a11867720f530ac42fe15bc18
Djordje <djordjev9@gmail.com>
2020-12-07 12:40:41 +0100

    Application properties and User

---
+++ src/main/java/horeca/host/User.java
@@ -0,0 +1,102 @@
+package horeca.host;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    private int active;
+    private String roles = "";
+    private String permissions = "";
+
+    protected User(){
+
+
+    }
+    public User(String username,
+                String password,
+                int active,
+                String roles,
+                String permissions) {
+        this.username = username;
+        this.password = password;
+        this.active = active;
+        this.roles = roles;
+        this.permissions = permissions;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getActive() {
+        return active;
+    }
+
+    public void setActive(int active) {
+        this.active = active;
+    }
+
+    public String getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String roles) {
+        this.roles = roles;
+    }
+
+    public String getPermissions() {
+        return permissions;
+    }
+
+    public void setPermissions(String permissions) {
+        this.permissions = permissions;
+    }
+
+    public List<String> getRoleList(){
+        if(this.roles.length() >0){
+            return Arrays.asList(this.roles.split(","));
+        }
+        return new ArrayList<>();
+    }
+    public List<String> getPermissionList(){
+        if(this.permissions.length() >0){
+            return Arrays.asList(this.permissions.split(","));
+        }
+        return new ArrayList<>();
+    }
+}
