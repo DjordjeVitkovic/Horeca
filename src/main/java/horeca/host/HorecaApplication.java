58c2fbde7a19b8286208918bb2a94de07351271d
Djordje <djordjev9@gmail.com>
2020-12-07 12:03:59 +0100

    First Commit
    Good luck

---
+++ src/main/java/horeca/host/HorecaApplication.java
@@ -0,0 +1,13 @@
+package horeca.host;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorecaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HorecaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/horeca/host/HorecaApplicationTests.java
@@ -0,0 +1,13 @@
+package horeca.host;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HorecaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
